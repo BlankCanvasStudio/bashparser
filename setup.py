a61ee075fe41917d18fe755d85380191d2c3e5f8
spencerstingley <laptop@bcs.com>
2021-11-10 14:20:05 -0800

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,32 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+import setuptools
 
-setup(name='bashparse',
-      version='1.0',
-      # list folders, not files
-      packages=['bashparse',
-                'bashparse.test'],
-      # scripts=['bashparse/bin/bashparse_script.py'],
-      # package_data={'bashparse': ['data/bashparse_data.txt']},
-      )
+with open("README.txt", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bashparse",
+    version="0.1",
+    author="YOU",
+    author_email="sstingle@usc.edu",
+    description="A tool for analysing and deobfuscating bash scripts",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/BlankCanvasStudio/bashparse",
+    packages=setuptools.find_packages(),
+    # entry_points={
+    #     'console_scripts': [
+    #         # migrating to pdb prefixes
+    #         'script-name = module.foo.bar:main',
+    #     ]
+    # },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires = '>=3.6',
+    install_requires=['bashlex'],
+    test_suite='nose.collector',
+    tests_require=['nose'],
+)
