c350e4e1f2b5bc192674f45ea53bc52e5da9b722
adam <spencerstingley6@gmail.com>
2022-12-17 11:33:59 -0800

    Shifted everything in AST to taking root as an optional parameter

--- bashparse/ast.py
+++ bashparse/ast.py
@@ -138,7 +138,7 @@ class NodeVisitor:
                 CONT - Continue searching the tree
                 HALT - Exit search 
         """
-        
+
         def run(node, *args, **kwargs):
             rt_val = apply_fn(node, *args, **kwargs)
             if rt_val == DONT_DESCEND: return CONT       
@@ -198,99 +198,102 @@ class NodeVisitor:
         return self.apply(apply_right_fn, apply_fn, path, self, *args, **kwargs)
 
 
-    def at_path(self, node = None, path = None):
-        """ Returns BY REFERENCE the node a path specified from the node passed in """
-        if node is None: node = self._root
+    def at_path(self, root = None, path = None):
+        """ Returns BY REFERENCE the node a path specified from the root passed in """
+        if root is None: root = self._root
         if path is None: path = self._path
-        if type(node) is not bashlex.ast.node: raise ValueError("Error! Invalid type. NodeVisitor.at_path(node != bashlex.ast.node)")
+        if type(root) is not bashlex.ast.node: raise ValueError("Error! Invalid type. NodeVisitor.at_path(root != bashlex.ast.node)")
         if type(path) is not list: raise ValueError("Error! Invalid type. NodeVisitor.at_path(path != list)")
-        for el in path: node = self.child(node, el)
-        return node
+        for el in path: root = self.child(root, el)
+        return root
 
 
-    def children(self, node = None):
-        """ Returns an array of all the children of either the node passed in or the root node of NodeVisitor if none specified. 
+    def children(self, root = None):
+        """ Returns an array of all the children of either the root passed in or the root node of NodeVisitor if none specified. 
             If there are no children, an empty array is returned"""
 
-        if node is None: node = self._root
-        k = node.kind
+        if root is None: root = self._root
+        k = root.kind
 
         if k in self.parts_children:
-            return node.parts
+            return root.parts
         elif k in self.no_children:
             return []
         elif k in self.command_children:
-            return [ node.command ]
+            return [ root.command ]
         elif k == 'compound':
-            if hasattr(node, 'list'):
-                return node.list
-            elif hasattr(node,'redirects'):
-                return [ node.redirects ]
+            if hasattr(root, 'list'):
+                return root.list
+            elif hasattr(root,'redirects'):
+                return [ root.redirects ]
         elif k == 'redirect':
-            if isinstance(node.output, bashlex.ast.node):
-                return [ node.output ]
-            if node.heredoc:
-                return [ node.heredoc[child_num] ]
-        elif hasattr(node, 'list'):
-            return node.list
+            if isinstance(root.output, bashlex.ast.node):
+                return [ root.output ]
+            if root.heredoc:
+                return [ root.heredoc[child_num] ]
+        elif hasattr(root, 'list'):
+            return root.list
         else:
             raise ValueError('unknown node kind %r' % k)
 
 
-    def child(self, node = None, num = 0):
+    def child(self, root = None, num = 0):
         """ Returns the child with number specified by argument 
-            ie can return the 3rd child of the root node (helpful for path finding) """
+            (ie can return the 3rd child of the root node (helpful for path finding) """
         # Could add error conditions here
-        if node is None: node = self._root
-        children = self.children(node)
+        if root is None: root = self._root
+        children = self.children(root)
         try:
             return children[num]
         except Exception as e:
             raise ValueError('Error! NodeVisitor.child #'+str(num)+' does not exist!')
 
 
-    def set_children(self, parent=None, children=None):
-        """ Takes a parent bashlex.ast.node and an array of bashlex.ast.node children.
+    def set_children(self, root=None, children=None):
+        """ Takes a root bashlex.ast.node and an array of bashlex.ast.node children.
             Returns the new node & children combo. Technically, BY REFERENCE not copy """
-        if parent is None: parent = self._root
+        if root is None: root = self._root
         if type(children) is not list: raise ValueError("NodeVisitor.children must be set to array of bashlex.ast.nodes")
         for tmp_n in children: 
             if type(tmp_n) is not bashlex.ast.node: raise ValueError("NodeVisitor.children must be set to array of bashlex.ast.nodes")
         
-        k = parent.kind
+        k = root.kind
 
         if k in self.parts_children:
-            parent.parts = children
+            root.parts = children
         elif len(children) and k in self.no_children:
             raise ValueError("Error! Node assigned children but necessarily has none")
         elif k in self.command_children:
             if len(children) != 1: raise ValueError("Error! Assigned multiple values to a Node's command attr")
-            parent.command = children[0]
+            root.command = children[0]
         elif k == 'compound':
-            if hasattr(parent, 'list'):
-                parent.list = children
-            elif hasattr(parent,'redirects'):
+            if hasattr(root, 'list'):
+                root.list = children
+            elif hasattr(root,'redirects'):
                 if len(children) != 1: raise ValueError("Error! Assigned multiple values to a Node's redirects attr")
-                parent.redirects = children[0]
+                root.redirects = children[0]
         elif k == 'redirect':
-            if isinstance(parent.output, bashlex.ast.node):
+            if isinstance(root.output, bashlex.ast.node):
                 if len(children) != 1: raise ValueError("Error! Assigned multiple values to a Node's output attr")
-                parent.output = children[0]
-            if parent.heredoc:
-                parent.heredoc = children
-        elif hasattr(parent, 'list'):
-            parent.list = children
+                root.output = children[0]
+            if root.heredoc:
+                root.heredoc = children
+        elif hasattr(root, 'list'):
+            root.list = children
         else:
             raise ValueError('unknown node kind %r' % k)
-        return parent
+        return root
 
 
-    def swap_node(self, root, path, new_child):
-        """ This replaces the node at the path specified with the new_child passed in
+    def swap_node(self, child, root = None, path = None):
+        """ This replaces the node at the path specified with the child passed in
             both of these actions are BY REFERENCE """
+        if root is None: root = self.root
+        if path is None: path = self.path()
+
         parent = self.at_path(root, path[:-1])
 
-        if not len(path): return new_child
+        if not len(path): return child
 
         try:
             old_child = self.child(parent, path[-1])
@@ -299,9 +302,9 @@ class NodeVisitor:
 
 
         """ Move the ast to account for the new node & adjst the new node """
-        self.justify(new_child)
-        self.align(new_child, old_child.pos[0])
-        delta = new_child.pos[1] - old_child.pos[1]
+        self.justify(child)
+        self.align(child, old_child.pos[0])
+        delta = child.pos[1] - old_child.pos[1]
         bashparse.ast.expand_ast_along_path(root, path[:-1], delta)
         bashparse.ast.shift_ast_right_of_path(root, path, delta)
 
@@ -310,38 +313,38 @@ class NodeVisitor:
 
         """ Actually inject the new child """
         if len(path) == 0:      
-            return new_child
+            return child
         elif k in self.parts_children:
             if len(parent.parts) >= path[-1]: 
-                parent.parts[path[-1]] = new_child
+                parent.parts[path[-1]] = child
         elif k in self.no_children:
             raise ValueError('Error! You are trying to replace the child of a node which, by definition, has no children. Function: bashparse.ast.NodeVisitor.replace')
         elif k in self.command_children:
             num_child_nodes = len(parent.command) 
             if num_child_nodes >= path[-1]:
-                parent.command[path[-1]] = new_child
+                parent.command[path[-1]] = child
         elif k == 'compound':
             if hasattr(root, 'list'):
                 num_child_nodes = len(parent.list)
                 if num_child_nodes >= path[-1]: 
-                    parent.list[path[-1]] = new_child
+                    parent.list[path[-1]] = child
             elif hasattr(parent,'redirects'):
                 child_nodes += 1
                 if path[-1] == 0:
-                    parent.redirect = new_child
+                    parent.redirect = child
         elif k == 'redirect':
             if isinstance(root.output, bashlex.ast.node):
                 num_child_nodes += 1
-                parent.output = new_child
+                parent.output = child
             if parent.heredoc:
                 num_child_nodes = len(parent.heredoc[path[-1]])
                 if num_child_nodes >= path[-1]: 
-                    parent.heredoc[path[-1]] = new_child
+                    parent.heredoc[path[-1]] = child
         else:
             raise ValueError('unknown node kind %r' % k)
         
         """ Error checking for sanity sake """
-        if self.child(parent, path[-1]) != new_child:
+        if self.child(parent, path[-1]) != child:
             raise ValueError('Error! bashparse.ast.NodeVisitor.replace did not change the node properly')
 
         return root
@@ -364,7 +367,7 @@ class NodeVisitor:
                 for replacement_node in replacement_nodes:
                     for self_node in self._nodes:
                         new_node = copy.deepcopy(self_node)
-                        tmp = self.swap_node(new_node, copy.deepcopy(self._path), replacement_node)
+                        tmp = self.swap_node(root=new_node, path=copy.deepcopy(self._path), child=replacement_node)
 
                         new_nodes += [ tmp ]
                 self._nodes = new_nodes
@@ -399,25 +402,25 @@ class NodeVisitor:
         return root
 
 
-    def align(self, node = None, delta = None):
+    def align(self, root = None, delta = None):
         """ This function shifts the pos attr of all nodes in self._root or node passed in by delta
             BY REFERENCE and returns the node for good measure """
-        if node is None: node = self._root
-        if type(node) is not bashlex.ast.node: raise ValueError("Error! NodeVisitor.align(node != bashlex.ast.node)")
+        if root is None: root = self._root
+        if type(root) is not bashlex.ast.node: raise ValueError("Error! NodeVisitor.align(root != bashlex.ast.node)")
         if type(delta) is not int: raise ValueError("Error! NodeVisitor.align(delta != int)")
-        def adjust_pos(node, delta):
-            start = (node.pos[0] + delta) if ((node.pos[0] + delta) > 0) else 0
-            node.pos = ( start, node.pos[1] + delta )
+        def adjust_pos(root, delta):
+            start = (root.pos[0] + delta) if ((root.pos[0] + delta) > 0) else 0
+            root.pos = ( start, root.pos[1] + delta )
             return CONT
-        NodeVisitor(root=node).apply(adjust_pos, delta)    
-        return node
+        NodeVisitor(root=root).apply(adjust_pos, delta)    
+        return root
 
 
-    def justify(self, node = None):
+    def justify(self, root = None):
         """ This function shifts the ast so it starts at zero. Basic wrapper around align. """
-        if node is None: node = self._root
-        if type(node) is not bashlex.ast.node: raise ValueError("Error! NodeVisitor.justify(node != bashlex.ast.node)")
-        return NodeVisitor(node).align(delta = -node.pos[0])
+        if root is None: root = self._root
+        if type(root) is not bashlex.ast.node: raise ValueError("Error! NodeVisitor.justify(root != bashlex.ast.node)")
+        return NodeVisitor(root).align(delta = -root.pos[0])
 
 
 
@@ -431,10 +434,10 @@ def expand_ast_along_path(root, path_to_update, delta):
     return NodeVisitor(root).apply_along_path(apply_fn, path_to_update, delta).root
 
 
-def shift_ast_right_of_path(node, path_to_update, delta):
+def shift_ast_right_of_path(root, path_to_update, delta):
     """ Takes a root and adds the delta to both pos attributes if its to the right of the path. Excludes the path.
-        Functions BY REFERENCE not value but returns the node for posterity sake """
-    if type(node) is not bashlex.ast.node: raise ValueError('node must be a bashparse.node')
+        Functions BY REFERENCE not value but returns the root for posterity sake """
+    if type(root) is not bashlex.ast.node: raise ValueError('root must be a bashparse.node')
     if type(path_to_update) is not list: raise ValueError('path_to_update must be a list')
     if type(delta) is not int: raise ValueError('delta must be an int')
 
@@ -442,4 +445,4 @@ def shift_ast_right_of_path(node, path_to_update, delta):
         new_start = node.pos[0] + delta if ((node.pos[0] + delta > 0) and (node.pos[0] != 0)) else 0
         new_end = node.pos[1] + delta
         node.pos = (new_start, new_end)
-    return NodeVisitor(node).apply_right_of_path(apply_fn, path_to_update, delta).root
+    return NodeVisitor(root).apply_right_of_path(apply_fn, path_to_update, delta).root
