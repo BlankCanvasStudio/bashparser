e84f9a27b9e8926d8a4e1e4ab18e3651161dae4d
eve <spencerstingley6@gmail.com>
2023-04-25 11:47:47 -0700

    Version bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="bashparser",
-    version="0.12",
+    version="0.13",
     author="Spencer Stingley",
     author_email="sstingle@usc.edu",
     description="A framework for manipulating and analysing bash scripts",
